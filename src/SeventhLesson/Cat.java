c5755e430f2db889e2416b4d25285badbab4f818
Strantonvad <nottna@gmail.com>
2020-01-26 20:43:04 +0300

    Домашнее задание №7

---
+++ src/SeventhLesson/Cat.java
@@ -0,0 +1,24 @@
+package SeventhLesson;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void eat(Plate p) {
+        if (p.getFood() > appetite) {
+            p.decreaseFood(appetite);
+            satiety = true;
+        }
+    }
+}
